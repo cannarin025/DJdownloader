76ae0e430790d0649a689fdf071579e77e8a4dd3
cannarin025 <cannarin025@gmail.com>
2022-11-14 22:42:53 +0000

    added pytube downloading for non soundcloud sources for faster downloads

--- song.py
+++ song.py
@@ -2,8 +2,10 @@ from PIL import Image
 from typing import Dict
 import os
 import requests
+import subprocess
 import eyed3
 from eyed3.id3.frames import ImageFrame
+from pytube import Stream
 
 
 def add_cover_art(filepath, art):
@@ -16,7 +18,7 @@ def add_cover_art(filepath, art):
     os.remove('cover.png')
 
 class Song:
-    def __init__(self, title: str, best_stream: Dict, cover_art: Image):
+    def __init__(self, title: str, best_stream: Stream or Dict, cover_art: Image):
         self.title = title
         self.best_stream = best_stream
         self.cover_art = cover_art
@@ -26,36 +28,15 @@ class Song:
             download_path = os.getcwd()
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.77'}
         print("Downloading!...")
-        audio = requests.get(self.best_stream['url'], headers, timeout=None)
-        filepath = f'{download_path}/{self.title}.mp3'
-        open(filepath, 'wb').write(audio.content)
-        print(f'Downloaded file as: {filepath}!')
-        
-        self.cover_art.save('test.png')
-        data = open('test.png', 'rb').read()
-        # add_cover_art(filepath, artpath='D:/Code/Projects/DJdownloader/test.png')
+        if type(self.best_stream) is dict:
+            audio = requests.get(self.best_stream['url'], headers, timeout=None)
+            filepath = f'{download_path}\\{self.title}.mp3'
+            open(filepath, 'wb').write(audio.content)
+            print(f'Downloaded file as: {filepath}!')
+        else:
+            self.best_stream.download(output_path=f'{download_path}')
+            download_filepath = f'{download_path}\\{[x for x in os.listdir(download_path) if self.title in x][0]}'
+            filepath = f'{download_path}\\{self.title}.mp3'
+            subprocess.run(['ffmpeg', '-i', download_filepath, filepath], stdout=subprocess.DEVNULL) # convert download to mp3 and suppress output
+            os.remove(download_filepath)
         add_cover_art(filepath, self.cover_art)
-
-        # audio = MP3(filepath, ID3=ID3)
-        # # add ID3 tag if it doesn't exist
-        # try:
-        #     audio.add_tags()
-        # except error:
-        #     pass
-        # audio.tags.add(
-        #     APIC(
-        #         encoding=3, # 3 is for utf-8
-        #         mime='image/png', # image/jpeg or image/png
-        #         type=3, # 3 is for the cover image
-        #         desc=u'Cover',
-        #         data=data
-        #     )
-        # )
-        # audio.save()
-
-        # audiofile = eyed3.load(filepath)
-        # if (audiofile.tag == None):
-        #     audiofile.initTag()
-        # #self.cover_art.tobytes()
-        # audiofile.tag.images.set(ImageFrame.FRONT_COVER, open('test.jpg', 'rb').read(), 'image/jpeg')
-        # audiofile.tag.save()
--- yt_downloader.py
+++ yt_downloader.py
@@ -2,45 +2,57 @@
 from typing import Dict, List
 from song import Song
 from youtube_dl import YoutubeDL
+from pytube import YouTube, Search
 from PIL import Image
 import requests
 import datetime
 
 
-def get_best_stream(streams: Dict) -> Dict:
+def get_best_stream_YoutubeDL(streams: Dict) -> Dict:
     best = streams[0]
     for stream in streams:
         if stream['abr'] > best['abr']:
             best = stream
     return stream
 
-def get_song(query: str, num_results = 10) -> Song:
+def get_song_YoutubeDL(url: str) -> Song:
     with YoutubeDL({'format':'bestaudio/best', 'noplaylist': 'True'}) as ydl:
+            video_data = ydl.extract_info(url, download=False) # url was supplied so video can be directly extracted
+            audio_streams = [x for x in video_data['formats'] if 'abr' in x.keys() and x['vcodec'] == 'none'] # select only audio streams
+            title = video_data['title']
+            cover_art = Image.open(requests.get(video_data['thumbnails'][-1]['url'], stream=True).raw)
+            best_stream = get_best_stream_YoutubeDL(audio_streams)
+    return Song(title, best_stream, cover_art)
+
+def get_song_pytube(video: YouTube) -> Song:
+    best_stream = video.streams.filter(type='audio').order_by('abr').last()  # Get highest quality stream
+    title = video.title
+    cover_art = Image.open(requests.get(video.thumbnail_url, stream=True).raw)
+    return Song(title, best_stream, cover_art)
+
+def get_song(query: str, num_results = 10) -> Song:
+    if 'SOUNDCLOUD' in query.upper(): # if link is to soundcloud use YouTubeDL
+        song = get_song_YoutubeDL(query)
+    else:
         try:
-            requests.get(query) # checks to see if query is a valid URL
-        except:
-            video_data_list = ydl.extract_info(f"ytsearch{num_results}:{query}", download=False)['entries'] # searches youtube with query string to get list of videos
-            for i, video_data in enumerate(video_data_list):
-                audio_streams = [x for x in video_data['formats'] if 'abr' in x.keys() and x['vcodec'] == 'none']
-                best_abr = audio_streams[0]['abr']
-                for stream in audio_streams:
-                    if stream['abr'] > best_abr:
-                        best_abr = stream['abr']
-                print(f"{i+1}. Title: {video_data['title']}\n{(len(str(i+1))+2) * ' '}Best abr: {best_abr}\n{(len(str(i+1))+2) * ' '}Duration: {str(datetime.timedelta(seconds=video_data['duration']))}\n")
+            requests.get(query) # a valid url is passed so get data directly from video
+            try:
+                video_data = YouTube(f"{query}")
+                song = get_song_pytube(video_data)
+            except:
+                raise Exception("There was an issue with grabbing the video from this URL!")
+        except: # query is a string so search youtube for suitable results
+            s = Search(query)
+            non_livestream_results = [video for video in s.results if 'reason' not in list(video.vid_info['playabilityStatus'].keys())] # ignore livestreams 
+            for i, video in enumerate(non_livestream_results):
+                best_abr = video.streams.filter(type='audio').order_by('abr').last().abr
+                print(f"{i+1}. Title: {video.title}\n{(len(str(i+1))+2) * ' '}Best abr: {best_abr}\n{(len(str(i+1))+2) * ' '}Duration: {str(datetime.timedelta(seconds=video.length))}\n")
             selected_index = int(input("Please enter a video index to continue: ")) - 1
-            video_data = video_data_list[selected_index]
-        else:
-            video_data = ydl.extract_info(query, download=False) # url was supplied so video can be directly extracted
-    
-    audio_streams = [x for x in video_data['formats'] if 'abr' in x.keys() and x['vcodec'] == 'none'] # select only audio streams
+            video = s.results[selected_index]
+            song = get_song_pytube(video)
+    return song 
 
-    title = video_data['title']
-    cover_art = Image.open(requests.get(video_data['thumbnails'][-1]['url'], stream=True).raw)
-    best_stream = get_best_stream(audio_streams)
-
-    return Song(title, best_stream, cover_art) 
-
-song = get_song('https://www.youtube.com/watch?v=c0PZxWO33_Q')
+# song = get_song('https://www.youtube.com/watch?v=c0PZxWO33_Q')
 # song = get_song('https://soundcloud.com/alpha-shitlord/jelly')
-# song = get_song('bustin makes me feel good')
+song = get_song('bustin makes me feel good')
 song.download()
