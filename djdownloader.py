7505f55f60bdf71a613d389516b2993442ddc610
cannarin025 <cannarin025@gmail.com>
2022-11-15 01:22:46 +0000

    artists can now be added to songs

--- djdownloader.py
+++ djdownloader.py
@@ -6,14 +6,18 @@ import requests
 parser = ArgumentParser(description="Downloads audio from YouTube or SoundCloud in best available quality")
 parser.add_argument("query")
 parser.add_argument("--dir", help="Directory to download files to. Will default to current directory.")
+parser.add_argument("--artists", help="Names of contributing artitsts. Separate with commas.")
 
 args = parser.parse_args()
 
 query = args.query
 download_dir = args.dir
+artists = args.artists
 
 if download_dir is None:
     download_dir = os.getcwd()
+if artists is not None:
+    artists = [x.strip() for x in artists.split(',')]
 
 try:
     requests.get(query) # query is a valid url
@@ -21,4 +25,4 @@ try:
 except:
     song = get_song_from_query(query) # query is not a valid URL so treat as search term
 
-song.download(download_path=download_dir)
+song.download(download_path=download_dir, artists=artists)
--- song.py
+++ song.py
@@ -6,9 +6,10 @@ import subprocess
 import eyed3
 from eyed3.id3.frames import ImageFrame
 from pytube import Stream
+from typing import List
 
 
-def add_cover_art(filepath, art):
+def add_cover_art(filepath: str, art) -> None:
     audiofile = eyed3.load(filepath)
     art.save('cover.png')
     if audiofile.tag is None:
@@ -17,13 +18,20 @@ def add_cover_art(filepath, art):
     audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
     os.remove('cover.png')
 
+def add_artists(filepath: str, artists: List[str]) -> None:
+    audiofile = eyed3.load(filepath)
+    if audiofile.tag is None:
+        audiofile.initTag()
+    audiofile.tag.artist = '/'.join(artists)
+    audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
+
 class Song:
     def __init__(self, title: str, best_stream: Stream or Dict, cover_art: Image):
         self.title = title
         self.best_stream = best_stream
         self.cover_art = cover_art
 
-    def download(self, download_path: str = ''):
+    def download(self, download_path: str = '', artists = None):
         if not download_path:
             download_path = os.getcwd()
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.77'}
@@ -40,4 +48,5 @@ class Song:
             subprocess.run(['ffmpeg', '-i', download_filepath, filepath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # convert download to mp3 and suppress output
             os.remove(download_filepath)
         add_cover_art(filepath, self.cover_art)
+        if artists: add_artists(filepath, artists)
         print(f"Done!\nFile downloaded at: {filepath}!")
